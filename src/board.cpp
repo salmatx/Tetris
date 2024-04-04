9cec817a8dd0e5f46d563a4cda356827b92831d9
salmatx <matus.salva@pm.me>
2024-04-04 19:27:10 +0200

    Init board class, add piece positioning in board validation

--- src/board.cpp
+++ src/board.cpp
@@ -1,1 +1,37 @@
+#include <cassert>
 #include "board.h"
+
+Board::Board(size_t width, size_t height) :
+        height_(height), width_(width),
+        board_(width, std::vector<uint8_t>(height)) {}
+
+Board::Board() : board_(width_, std::vector<uint8_t>(height_)) {}
+
+void Board::SetValue(const int& row, const int& col, const uint8_t value) {
+    this->board_.at(col).at(row) = value;
+}
+
+uint8_t Board::GetValue(const int& row, const int& col) {
+    return this->board_.at(col).at(row);
+}
+
+bool Board::CheckPieceValid(const Board::PieceState& piece) {
+    assert(&piece.piece);
+    tetrino* shape = piece.piece.GetPiece();
+    uint16_t size = piece.piece.GetSize();
+    for (int i = 0; i < size; ++i) {
+        for (int j = 0; j < size; ++j) {
+            uint8_t value = *shape++;
+            if (value > 0) {
+                int board_row = piece.offset_row + i;
+                int board_col = piece.offset_row + j;
+                if (board_row < 0) return false;
+                if (board_row >= this->height_) return false;
+                if (board_col < 0) return false;
+                if (board_col >= this->width_) return false;
+                if (this->GetValue(board_row, board_col)) return false;
+            }
+        }
+    }
+    return true;
+}
--- src/board.h
+++ src/board.h
@@ -1,5 +1,27 @@
 #pragma once
 
-class Board {
+#include "piece.h"
 
+#include <cstddef>
+#include <vector>
+#include <cstdint>
+
+class Board {
+public:
+    Board();
+    Board(size_t width, size_t height);
+
+private:
+    struct PieceState {
+        Piece piece;
+        int offset_row;
+        int offset_col;
+    };
+    const size_t height_ = 22;
+    const size_t width_ = 10;
+    std::vector<std::vector<uint8_t>> board_;
+
+    void SetValue(const int& row, const int& col, const uint8_t value);
+    uint8_t GetValue(const int& row, const int& col);
+    bool CheckPieceValid(const PieceState& piece);
 };
