eb5ed26a4d9a57a7176e4caa5ebd5309db84c083
salmatx <matus.salva@pm.me>
2024-04-06 23:40:17 +0200

    Move Shape enum to common, add namaspace

--- src/common.h
+++ src/common.h
@@ -1,5 +1,13 @@
 #pragma once
 
+namespace game {
+
 enum class MoveTypes {
     kLeft, kRight, kRotate, kFastDrop, kHardDrop
 };
+
+enum class Shape {
+    kSquare, kBar, kPyramid, kSShape, kZShape, kLShape, kJShape, kNumOfShapes
+};
+
+}
