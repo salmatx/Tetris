7cc821de7e2b05a73e63aaf2a5ef58254434d0fd
salmatx <matus.salva@pm.me>
2024-04-07 13:45:55 +0200

    Add basic movement

--- src/game.cpp
+++ src/game.cpp
@@ -1,14 +1,48 @@
+#include <chrono>
 #include "game.h"
 
 namespace game {
 
 Game::Game(int left_move, int right_move, int rotation, int fast_drop,
-           int hard_drop) {
+           int hard_drop) : board_(std::make_unique<Board>()) {
     this->moves_.left = left_move;
     this->moves_.right = right_move;
     this->moves_.rotate = rotation;
     this->moves_.fast_drop = fast_drop;
     this->moves_.hard_drop = hard_drop;
+
+    this->start_time_ = std::chrono::steady_clock::now();
 }
 
+void Game::SetBoardSize(const size_t width, const size_t height) {
+    this->board_ = std::make_unique<Board>(width, height);
+}
+
+void Game::UpdateGameplay(const MoveTypes& input) {
+    this->board_->MovePiece(input);
+    while (this->time_duration_ >= this->next_drop_time_) {
+        this->DropPiece();
+    }
+}
+
+float Game::GetTimeToNextDrop() {
+    if (this->level_ > 29) {
+        this->level_ = 29;
+    }
+    return this->kFramesPerDrop[this->level_] * this->kTargetSecondsPerFrame;
+}
+
+void Game::DropPiece() {
+    if (this->board_->SoftDrop()) {
+        this->next_drop_time_ = this->time_duration_ + this->GetTimeToNextDrop();
+    }
+
+}
+
+void Game::UpdateGame(const MoveTypes& input) {
+    this->current_time_ = std::chrono::steady_clock::now();
+    this->time_duration_ =
+            std::chrono::duration_cast<std::chrono::duration<float>>
+                    (this->current_time_ -this->start_time_).count();
+}
 }
--- src/game.h
+++ src/game.h
@@ -1,18 +1,18 @@
 #pragma once
 
+#include "common.h"
+#include "board.h"
+
 #include <cstddef>
 
 namespace game {
 
 class Game {
 public:
-    enum class InputTypes {
-        kLeft, kRight, kRotate, kFastDrop, kHardDrop
-    };
-
     Game(int left_move, int right_move, int rotation, int fast_drop,
          int hard_drop);
-    void GameLoop(const InputTypes& input);
+    void UpdateGame(const MoveTypes& input);
+    void SetBoardSize(const size_t width, const size_t height);
 
 private:
     enum class GamePhase {
@@ -27,12 +27,26 @@ private:
         int hard_drop;
     };
 
+    const uint8_t kFramesPerDrop[30]{
+            48, 43, 38, 33, 28, 23, 18, 13, 8, 6,
+            5, 5, 5, 4, 4, 4, 3, 3, 3, 2,
+            2, 2, 2, 2, 2, 2, 2, 2, 2, 1
+    };
+    const float kTargetSecondsPerFrame = 1.0f / 60.0f;
+
+    std::unique_ptr<Board> board_;
     Moves moves_;
     size_t points_ = 0;
     size_t level_ = 0;
     GamePhase game_phase_ = GamePhase::kStartPhase;
+    float next_drop_time_;
+    float time_duration_;
+    std::chrono::time_point<std::chrono::steady_clock> start_time_;
+    std::chrono::time_point<std::chrono::steady_clock> current_time_;
 
-    void UpdateGameplay(const InputTypes& input);
+    void UpdateGameplay(const MoveTypes& input);
+    float GetTimeToNextDrop();
+    void DropPiece();
 };
 
 }
