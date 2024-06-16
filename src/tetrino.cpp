d5bf4b93de3fa4573e0c5c7c78f53b254e7db488
salmatx <matus.salva@pm.me>
2024-06-16 14:22:22 +0200

    Change shared_ptr creation to backward compatibility with older GCC

--- src/tetrino.cpp
+++ src/tetrino.cpp
@@ -8,7 +8,7 @@ Tetrino::Tetrino(Shape shape) {
     switch (shape) {
         case Shape::kSquare: {
             this->instance_ = std::make_shared<TetrinoPiece>(
-                    std::make_shared<tetrino[]>(4), 2);
+                    std::shared_ptr<tetrino[]>(new tetrino[4]), 2);
             tetrino tmp[]{1, 1,
                           1, 1};
             std::ranges::copy(tmp, this->instance_->shape.get());
@@ -16,7 +16,7 @@ Tetrino::Tetrino(Shape shape) {
         }
         case Shape::kBar: {
             this->instance_ = std::make_shared<TetrinoPiece>(
-                    std::make_shared<tetrino[]>(16), 4);
+                    std::shared_ptr<tetrino[]>(new tetrino[16]), 4);
             tetrino tmp[]{0, 0, 0, 0,
                           2, 2, 2, 2,
                           0, 0, 0, 0,
@@ -26,7 +26,7 @@ Tetrino::Tetrino(Shape shape) {
         };
         case Shape::kPyramid: {
             this->instance_ = std::make_shared<TetrinoPiece>(
-                    std::make_shared<tetrino[]>(9), 3);
+                    std::shared_ptr<tetrino[]>(new tetrino[9]), 3);
             tetrino tmp[]{0, 0, 0,
                           3, 3, 3,
                           0, 3, 0};
@@ -35,7 +35,7 @@ Tetrino::Tetrino(Shape shape) {
         }
         case Shape::kSShape: {
             this->instance_ = std::make_shared<TetrinoPiece>(
-                    std::make_shared<tetrino[]>(9), 3);
+                    std::shared_ptr<tetrino[]>(new tetrino[9]), 3);
             tetrino tmp[]{0, 4, 4,
                           4, 4, 0,
                           0, 0, 0};
@@ -44,7 +44,7 @@ Tetrino::Tetrino(Shape shape) {
         }
         case Shape::kZShape: {
             this->instance_ = std::make_shared<TetrinoPiece>(
-                    std::make_shared<tetrino[]>(9), 3);
+                    std::shared_ptr<tetrino[]>(new tetrino[9]), 3);
             tetrino tmp[]{5, 5, 0,
                           0, 5, 5,
                           0, 0, 0};
@@ -53,7 +53,7 @@ Tetrino::Tetrino(Shape shape) {
         }
         case Shape::kLShape: {
             this->instance_ = std::make_shared<TetrinoPiece>(
-                    std::make_shared<tetrino[]>(9), 3);
+                    std::shared_ptr<tetrino[]>(new tetrino[9]), 3);
             tetrino tmp[]{0, 6, 0,
                           0, 6, 0,
                           0, 6, 6};
@@ -62,7 +62,7 @@ Tetrino::Tetrino(Shape shape) {
         }
         case Shape::kJShape: {
             this->instance_ = std::make_shared<TetrinoPiece>(
-                    std::make_shared<tetrino[]>(9), 3);
+                    std::shared_ptr<tetrino[]>(new tetrino[9]), 3);
             tetrino tmp[]{0, 7, 0,
                           0, 7, 0,
                           7, 7, 0};
